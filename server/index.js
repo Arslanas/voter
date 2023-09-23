dfb221a106f1df444a38312d661ce2ffc4d972c1
arslan <ovezberdyevarslan@gmail.com>
2023-09-23 14:27:39 +0900

    Add node js backend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,3 @@ app.listen(PORT, () => {
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
-
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
