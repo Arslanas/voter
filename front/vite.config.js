8d1e1965a2b0afdade4e747bed8d52df7c1dc2b9
Arslan <ovezberdyevarslan@gmail.com>
2025-12-06 09:32:04 +0900

    master :

--- front/vite.config.js
+++ front/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         port: 3000,
         open: true, // Optional: automatically opens the browser
         proxy: {
-            '/': {
+            '/api': {
                 target: 'http://localhost:3001',
                 changeOrigin: true,
                 secure: false,
